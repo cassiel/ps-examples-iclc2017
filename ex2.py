7f50f9a7f5cf27bf896d532a7b55d4f0be67aa46
Nick (Git client) <nick@cassiel.com>
2017-11-24 12:21:54 +0000

    Example 2.

--- ex2.py
+++ ex2.py
@@ -1,4 +1,4 @@
-# ex1: starting example: simple cyclic sequencing.
+# ex2: transposition; velocities and durations.
 
 # Wheel in the pulse sequencer itself:
 import sys, os
@@ -9,7 +9,7 @@ if not PS_DIR in sys.path:
 
 from core.interfacing import Outputter
 from core.basis import Context
-from lib.chains import Assembler
+from lib.chains import Assembler, Transposer, Ranger, Indexer, Selector, Atom
 from lib.pulses import Sprayer, Cycler
 
 # We need a "context" to do some book-keeping as we build chains and sequences:
@@ -20,19 +20,39 @@ c = Context()
 outputter = Outputter(maxObject, c, 0, 64, 100)
 
 # Let's assemble some pitches as a chain:
-P = Assembler(c, 58, 60, 63, 53, 65)
+P = Assembler(c, 60, 62, 63, 65, 67)
 
-# We need a pulse which will plant a pitch, and then emit a note:
-fan = Sprayer(c, outputter.pitch, outputter.emit)
+# I want to transpose that down an octave:
+P_12 = Transposer(c, P, -12)
+
+# (Here, '-12' is a chain.)
+
+# And just because we can, let's concatenate them:
+P_all = Assembler(c, P, P_12)
+
+# Now, let's look at velocities: we'll cycle these three values:
+vels = [20, 60, 127]
+
+# (In this simple case we can just write the list.)
+
+# Here's a pulse which cycles them (regardless of what we send it):
+vel_P = Cycler(c, vels, outputter.velocity, firstIf='', nextIf='..', loopIf='..')
+
+# Ditto for durations:
+durs = [50, 150]
+dur_P = Cycler(c, durs, outputter.duration, firstIf='', nextIf='..', loopIf='..')
+
+# (Note the '' for firstIf: never reset. '..' would be always reset.)
+
+# We need a pulse which will plant a pitch, kick the velocity generator, and then emit a note:
+fan = Sprayer(c, outputter.pitch, vel_P, dur_P, outputter.emit)
 
 # We need a pulse which will take the clock messages and cycle along the fan:
-input = Cycler(c, P, fan, firstIf=[0, 0], nextIf=[None, None], loopIf=[None, None])
+input = Cycler(c, P_all, fan, firstIf=0, nextIf='..', loopIf='..')
 
 # (Q: what happens if you send clocks directly to the fan?)
 
 # Hint: we can modify firstIf, nextIf etc. to get different looping behaviour.
-
-# Also, shortcuts: [0, 0] -> 0, [None, None] -> '..'.
 
 # All done. Send the clock counter messages to the input pulse:
 def clock(i):
